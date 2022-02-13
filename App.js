33ea202466fb2783a9c26d0f725e8f923e87eb20
Alvin Yanson <yansonalvin29@gmail.com>
2022-02-13 16:17:45 +0800

    add todo

--- App.js
+++ App.js
@@ -2,17 +2,27 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
-   
+import AddTodo from './components/addTodo';
+
 export default function App() {
   const [todos, setTodos] = useState([
     { text: 'buy coffee', key: '1' },
     { text: 'create an app', key: '2' },
     { text: 'play on the switch', key: '3' }
-  ]); 
+  ]);
 
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
       return prevTodos.filter(todo => todo.key != key);
+    })
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString() },
+        ...prevTodos,
+      ]
     })
   }
 
@@ -21,6 +31,8 @@ export default function App() {
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
+
         {/* add todo form */}
         <View style={styles.list}>
           <FlatList
